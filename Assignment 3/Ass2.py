3a7864d183d62c3e63f2c45d16cc3dca3663b63d
Rutik Kale <117524955+rutikbkale@users.noreply.github.com>
2024-02-20 16:33:11 +0530

    Delete Assignment 3 directory

--- Assignment 3/Ass2.py
+++
@@ -1,9 +0,0 @@
-# WAP to append new content to an existing one.
-
-with open("sample.txt", "a") as file:
-    file.write("\nWelcome")
-
-with open("sample.txt", "r") as file1:
-    print(file1.read())
-file.close()
-file1.close()
